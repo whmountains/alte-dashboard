11c1efa6d3ab5c547096e50617dde4efcc6f6a42
Caleb Whiting <whmountains@icloud.com>
2016-03-18 17:16:06 -0300

    enable morgan for request logging

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var http     = require('http')
 var socketio = require('socket.io')
 var debug    = require('debug')
 var inputs   = require('./reader.js')
+var morgan   = require('morgan')
 
 var app = express()
 var server = http.Server(app)
 var io = socketio(server)
 var log = debug('tmon:app.js')
 
+app.use(morgan('dev'))
 app.use(express.static('public'))
 
 setInterval(readSensors, 1000)
